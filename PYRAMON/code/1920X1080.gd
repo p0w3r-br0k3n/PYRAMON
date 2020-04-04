b2b6da45ce5ee1965e91a7bab463e39e3e60f7b1
PaulosK <pkpaulosk@gmail.com>
2020-04-04 21:48:33 +0300

    4/4/2020
    +added fullscreen option
    +added custom resolution option
    -Can we make an option button that will have all the resolutions we want to add?

---
+++ PYRAMON/code/1920X1080.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_1920X1080_pressed():
+	 OS.window_size=Vector2(1920 ,1080)
---
+++ PYRAMON/code/fullscreen.gd
@@ -0,0 +1,6 @@
+extends CheckButton
+
+
+
+func _on_CheckButton_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
