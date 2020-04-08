fa08271f0edc005f83cc2502f2776de8ee086dbb
Kimplul <please_don't@IHaveNoWorkEmail.com>
2020-04-08 21:56:39 +0300

    additional small fixes regarding the main menu

--- PYRAMON/code/play_map.gd
+++ PYRAMON/code/play_map.gd
@@ -1,6 +1,7 @@
 extends TextureButton
 onready var lets_a_go = get_node("/root/map_picker/lets_a_go_desc")
 onready var map = load("res://scenes/dialogue.tscn")
+onready var startui = get_node("/root/StartUI")
 onready var map_picker = get_node("/root/map_picker")
 
 func _ready():
@@ -10,5 +11,5 @@ func _on_play_map_pressed():
 	var num = lets_a_go.get_let_go()
 	if num == 1:
 		get_tree().root.add_child(map.instance())
-		get_parent().get_parent().remove_child(get_parent())
-		queue_free()
+		startui.queue_free()
+		get_parent().queue_free()
