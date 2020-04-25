dfc871ac3213c5f1c9cc8856526f5ce28e0c98a3
Kimplul <please_don't@IHaveNoWorkEmail.com>
2020-04-25 18:27:41 +0300

    various bugfixes

--- PYRAMON/code/hand_swervel.gd
+++ PYRAMON/code/hand_swervel.gd
@@ -6,6 +6,9 @@ var ray_target=Vector3()
 onready var cam = get_node("/root/level/InterpolatedCamera")
 
 func _physics_process(delta):
+	ray_origin = cam.project_ray_origin(get_viewport().get_mouse_position())
+	ray_target = cam.project_ray_normal(get_viewport().get_mouse_position())*1000
+	
 	var space_state = get_world().direct_space_state
 	var ray = space_state.intersect_ray(ray_origin,ray_target,[self],1,true,true)
 	
@@ -15,10 +18,3 @@ func _physics_process(delta):
 		ray_collision_point = ray_collision_point - object_position
 		var angle = -Vector2(ray_collision_point.x, ray_collision_point.y).angle_to(Vector2(1, 0))
 		self.set_rotation(Vector3(angle,0,0))
-
-func _input(event):
-
-	if event is InputEventMouseMotion:
-		ray_origin = cam.project_ray_origin(get_viewport().get_mouse_position())
-		ray_target = cam.project_ray_normal(get_viewport().get_mouse_position())*1000
-		
