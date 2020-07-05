ab851cc32b07268b07854f84f47b636933d67528
PaulosK <pkpaulosk@gmail.com>
2020-07-05 20:29:37 +0300

    ADDED STARTUI BCG

--- PYRAMON/code/ENEMY_1.gd
+++ PYRAMON/code/ENEMY_1.gd
@@ -1,15 +1,17 @@
 extends KinematicBody
+onready var Player = get_parent().get_node("Player")
 var space_state
 var target
 const speed = 300
+var dir = 0
 var pos = Vector3(0, 0, 0)
-const gravity = 981
+const gravity = 600
 var last_trans = translation
 var accel = 0
 var new_pos= Vector3(0,0,0)
 var health=10
 onready var sho = get_tree().get_root().get_node("/root/level/enem/hand_swervel/hand_right/scene_root")
-const leg_force= 500
+const leg_force= 600
 const forward_jump = 300
 var physics_delta = 0;
 var vel= Vector3(0,0,0)
@@ -23,7 +25,9 @@ func get_translation_delta():
 	return delta
 # warning-ignore:unused_argument
 func _physics_process(delta):
-	pass
+	if health <=0:
+		hide()
+		$respawn.start()
 
 func _process(delta):
 	if pos != new_pos:
@@ -32,9 +36,8 @@ func _process(delta):
 	if sho.reload == 0:
 		$AnimationPlayer.play("relaoad_pistol_hands")
 		
-	if health==0:
-		queue_free()
-	move_and_slide(vel*delta)
+
+
 	if target:
 		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
 		if result.collider:
@@ -53,10 +56,11 @@ func move_to_target(delta):
 		$AnimationPlayer.play("walking")
 	elif speed<0:
 		$AnimationPlayer.play("walking_left")
-	vel.y = (-gravity)
-	vel.x = speed
-	move_and_slide(direction*vel*delta,Vector3(0, 1 ,0), false, 4, 0.785398, true)
+	vel.y += -gravity * delta;
+	vel.x = dir * speed
 
+	vel = move_and_slide(vel, Vector3(0, -1,0))
+	
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
 		target= body
@@ -69,4 +73,8 @@ func _on_Area_body_exited(body):
 		print(2)
 
 func hurt():
-	print("Ow! That hurt!")
+	health=health-10
+
+func _on_respawn_timeout():
+	print("tome")
+	$respawn.stop()
